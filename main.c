c4126ff6fed2e9735b904c624c17e94b2b33d7ff
Tahmeed <tahmeedahmed06@gmail.com>
2026-07-30 19:51:27 +0600

    Showed CGPA in main

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
-#include <stdio.h> 
-#include "course.h" 
-#include "courseResult.h"
+#include <stdio.h>
+ #include "course.h" 
+ #include "courseResult.h" 
+ #include "gpa.h"
 
 int main()
 {
@@ -15,10 +16,13 @@ printf("Marks for %s: ", courses[i].name); scanf("%lf", &marks);
 results[i] = createCourseResult(&courses[i], marks);
 }
 
+printf("\nResults\n");
 for (int i = 0; i < n_courses; i++)
 {
 viewCourseResult(results[i]);
+printf("Grade: %s\n", getLetterGrade(results[i]));
 }
+printf("CGPA: %.2f\n", calculateGPA(results, n_courses));
 
 return 0;
 }
